317d81c871ad1ed712edad37a4967f81343884b8
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2021-05-14 08:51:56 +0400

    Change time for display.

--- client/main.lua
+++ client/main.lua
@@ -123,7 +123,7 @@ AddEventHandler("master_chat:reciveMessage", function(message)
 		if(Vdist(playerCoords.x, playerCoords.y, playerCoords.z, playerCoords2.x, playerCoords2.y, playerCoords2.z) < message.range) then
 			
 			if message.message_type == 'local' then
-				exports.pNotify:SendNotification({text = message.name .. ": " .. message.message, type = "info", layout = 'centerLeft', timeout = 1500})
+				exports.pNotify:SendNotification({text = message.name .. ": " .. message.message, type = "info", layout = 'centerLeft', timeout = 4000})
 			end
 			
 			message.message = message.message:gsub("%^[0-9]", "")
